7ddf8e16e96d9cc5ffee2c3e65aff89c1b2bde10
Taka Goto <tgoto@8thlight.com>
2022-04-07 18:58:59 -0500

    update with nano suits

--- deploy-commands.js
+++ deploy-commands.js
@@ -17,14 +17,81 @@ const genesis =	new SlashCommandBuilder()
 	  	option.setName('tokenid')
 	  		.setDescription('tokenid of your nft')
 	  		.setRequired(true));
+const izzy =	new SlashCommandBuilder()
+	  .setName('izzy')
+	  .setDescription('return phone screen img of your 0n1 with izzy r0n1')
+	  .addStringOption(option =>
+	  	option.setName('tokenid')
+	  		.setDescription('tokenid of your nft')
+	  		.setRequired(true));
+const nanoAo =	new SlashCommandBuilder()
+	  .setName('nano-ao')
+	  .setDescription('return your 0n1 with Ao nano suit')
+	  .addStringOption(option =>
+	  	option.setName('tokenid')
+	  		.setDescription('tokenid of your nft')
+	  		.setRequired(true));
+const nanoAka =	new SlashCommandBuilder()
+	  .setName('nano-aka')
+	  .setDescription('return your 0n1 with Aka nano suit')
+	  .addStringOption(option =>
+	  	option.setName('tokenid')
+	  		.setDescription('tokenid of your nft')
+	  		.setRequired(true));
+const nanoKiiro =	new SlashCommandBuilder()
+	  .setName('nano-kiiro')
+	  .setDescription('return your 0n1 with Kiiro nano suit')
+	  .addStringOption(option =>
+	  	option.setName('tokenid')
+	  		.setDescription('tokenid of your nft')
+	  		.setRequired(true));
+const nanoObsidianGold =	new SlashCommandBuilder()
+	  .setName('nano-obsidian-gold')
+	  .setDescription('return your 0n1 with Obsidian Gold nano suit')
+	  .addStringOption(option =>
+	  	option.setName('tokenid')
+	  		.setDescription('tokenid of your nft')
+	  		.setRequired(true));
+const nanoObsidianAo =	new SlashCommandBuilder()
+	  .setName('nano-obsidian-ao')
+	  .setDescription('return your 0n1 with Obsidian Ao nano suit')
+	  .addStringOption(option =>
+	  	option.setName('tokenid')
+	  		.setDescription('tokenid of your nft')
+	  		.setRequired(true));
+const nanoPearl =	new SlashCommandBuilder()
+	  .setName('nano-pearl')
+	  .setDescription('return your 0n1 with Pearl nano suit')
+	  .addStringOption(option =>
+	  	option.setName('tokenid')
+	  		.setDescription('tokenid of your nft')
+	  		.setRequired(true));
+const suitTease =	new SlashCommandBuilder()
+	  .setName('warning')
+	  .setDescription('suit teaser on your 0n1')
+	  .addStringOption(option =>
+	  	option.setName('tokenid')
+	  		.setDescription('tokenid of your nft')
+	  		.setRequired(true));
 const commands = [
   ph0n1,
-  genesis
-]
-	.map(command => command.toJSON());
+  genesis,
+  izzy,
+  nanoAo,
+  nanoAka,
+  nanoKiiro,
+  nanoObsidianGold,
+  nanoObsidianAo,
+  nanoPearl
+//  suitTease
+].map(command => command.toJSON());
 
 const rest = new REST({ version: '9' }).setToken(token);
 
 rest.put(Routes.applicationGuildCommands(clientId, guildId), { body: commands })
-	.then(() => console.log('Successfully registered application commands.'))
+	.then(() => console.log('Successfully registered application commands to 0n1 force.'))
+	.catch(console.error);
+
+rest.put(Routes.applicationGuildCommands(clientId, "941057715722149958"), { body: commands })
+	.then(() => console.log('Successfully registered application commands to missing-nin.'))
 	.catch(console.error);
--- index.js
+++ index.js
@@ -22,6 +22,12 @@ client.on('interactionCreate', async interaction => {
 		await interaction.reply(url);
 	}
 
+	if (commandName === 'izzy') {
+    tokenId = +tokenId
+    const url = `https://regentool.mypinata.cloud/ipfs/QmPLxxvzKPLMGDHQoAdo2ZHx7VHHUdf342tbQjyVmi9VXm/0n1_izzy_${tokenId}.png`
+		await interaction.reply(url);
+	}
+
   if (commandName === "genesis") {
     if (tokenId.length < 4) {
       for (let i = tokenId.length; i < 4; i++) {
@@ -29,7 +35,44 @@ client.on('interactionCreate', async interaction => {
       }
     }
     const url = `https://ipfs.io/ipfs/QmcoavNZq2jyZGe2Zi4nanQqzU9hRPxunHAo8pgYZ5fSep/${tokenId}.png`
+		await interaction.reply(url);
   }
-});
 
+//  if (commandName === "warning") {
+//    tokenId = +tokenId
+//    const url = `https://regentool.mypinata.cloud/ipfs/QmSPgDD5ERvUPtbhNNnCkXA33vna5AXuVYmFZ2nXpxD6ia/0n1_suit_teaser_${tokenId}.png`
+//		await interaction.reply(url);
+//  }
+  var nanoSuitHash = "QmeKo9m435po4bd7HEqgwd2E1g4imioNcy7RtggW3rHJaV"
+	if (commandName === 'nano-ao') {
+    tokenId = +tokenId
+    const url = `https://regentool.mypinata.cloud/ipfs/${nanoSuitHash}/nano_suit_${tokenId}_4.png`
+		await interaction.reply(url);
+	}
+	if (commandName === 'nano-aka') {
+    tokenId = +tokenId
+    const url = `https://regentool.mypinata.cloud/ipfs/${nanoSuitHash}/nano_suit_${tokenId}_6.png`
+		await interaction.reply(url);
+	}
+	if (commandName === 'nano-kiiro') {
+    tokenId = +tokenId
+    const url = `https://regentool.mypinata.cloud/ipfs/${nanoSuitHash}/nano_suit_${tokenId}_5.png`
+		await interaction.reply(url);
+	}
+	if (commandName === 'nano-obsidian-ao') {
+    tokenId = +tokenId
+    const url = `https://regentool.mypinata.cloud/ipfs/${nanoSuitHash}/nano_suit_${tokenId}_3.png`
+		await interaction.reply(url);
+	}
+	if (commandName === 'nano-obsidian-gold') {
+    tokenId = +tokenId
+    const url = `https://regentool.mypinata.cloud/ipfs/${nanoSuitHash}/nano_suit_${tokenId}_1.png`
+		await interaction.reply(url);
+	}
+	if (commandName === 'nano-pearl') {
+    tokenId = +tokenId
+    const url = `https://regentool.mypinata.cloud/ipfs/${nanoSuitHash}/nano_suit_${tokenId}_2.png`
+		await interaction.reply(url);
+	}
+});
 client.login(token);
